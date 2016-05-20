2432be0524f62ad41aae5aba653ee1c8086bf73e
ogursoft <ogu@fromru.com>
2016-05-20 16:31:31 +0300

    Изменено вывод температуры восприятия

--- weather_station.ino
+++ weather_station.ino
@@ -1,4 +1,4 @@
-#include <Wire.h> 
+#include <Wire.h>
 #include <BMP085.h>
 #include <OneWire.h>
 
@@ -7,19 +7,19 @@
 #define DHTPIN 6     // what pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
 
-BMP085 dps = BMP085();   
+BMP085 dps = BMP085();
 OneWire  ds(2);  // on pin 2 (a 4.7K resistor is necessary)
 DHT dht(DHTPIN, DHTTYPE);
 
 long Temperature = 0, Pressure = 0, Altitude = 0;
 
 void setup() {
-  Serial.begin(9600); 
-  Wire.begin(); 
+  Serial.begin(9600);
+  Wire.begin();
   dht.begin();
   delay(1000);
 
-  dps.init(MODE_STANDARD, 0, true);  // 132 meters, true = using meter units 
+  dps.init(MODE_STANDARD, 0, true);  // 132 meters, true = using meter units
 
 }
 
@@ -30,7 +30,7 @@ void loop() {
   byte data[12];
   byte addr[8];
   float celsius, fahrenheit;
-  
+
   if ( !ds.search(addr)) {
     Serial.println("No more addresses.");
     Serial.println();
@@ -38,19 +38,19 @@ void loop() {
     delay(250);
     return;
   }
-  
+
   Serial.print("ROM =");
-  for( i = 0; i < 8; i++) {
+  for ( i = 0; i < 8; i++) {
     Serial.write(' ');
     Serial.print(addr[i], HEX);
   }
 
   if (OneWire::crc8(addr, 7) != addr[7]) {
-      Serial.println("CRC is not valid!");
-      return;
+    Serial.println("CRC is not valid!");
+    return;
   }
   Serial.println();
- 
+
   // the first ROM byte indicates which chip
   switch (addr[0]) {
     case 0x10:
@@ -68,17 +68,17 @@ void loop() {
     default:
       Serial.println("Device is not a DS18x20 family device.");
       return;
-  } 
+  }
 
   ds.reset();
   ds.select(addr);
   ds.write(0x44, 1);        // start conversion, with parasite power on at the end
-  
+
   delay(1000);     // maybe 750ms is enough, maybe not
   // we might do a ds.depower() here, but the reset will take care of it.
-  
+
   present = ds.reset();
-  ds.select(addr);    
+  ds.select(addr);
   ds.write(0xBE);         // Read Scratchpad
 
   Serial.print("  Data = ");
@@ -114,56 +114,60 @@ void loop() {
   }
   celsius = (float)raw / 16.0;
   fahrenheit = celsius * 1.8 + 32.0;
-   
+
   //давление bmp180
-  dps.getPressure(&Pressure); 
-  dps.getAltitude(&Altitude); 
+  dps.getPressure(&Pressure);
+  dps.getAltitude(&Altitude);
   dps.getTemperature(&Temperature);
 
-//влажность
-// Reading temperature or humidity takes about 250 milliseconds!
+  //влажность
+  // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
   // Read temperature as Celsius
   float t = dht.readTemperature();
   // Read temperature as Fahrenheit
   float f = dht.readTemperature(true);
-  
+
   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t) || isnan(f)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
-  
+
   // Compute heat index
-  // Must send in temp in Fahrenheit!
-  float hi = dht.computeHeatIndex(f, h);
+  // in Fahrenheit!
+  float hif = dht.computeHeatIndex(f, h);
+  // по Цельсию!
+  float hic = dht.computeHeatIndex(t, h, false);
 
   //вывод данных
-//с датчика ds18b20
+  //с датчика ds18b20
   Serial.print("  Temperature = ");
   Serial.print(celsius);
   Serial.print(" Celsius, ");
   Serial.print(fahrenheit);
-  Serial.println(" Fahrenheit"); 
-//с датчика bmp180
-  Serial.print("  Alt(m):"); 
-  Serial.print(Altitude/100); 
-  Serial.print("  Pressure(mm Hg):"); 
-  Serial.print(Pressure/133.3);
-  Serial.print("  Temp:"); 
-  Serial.println(Temperature*0.1);
+  Serial.println(" Fahrenheit");
+  //с датчика bmp180
+  Serial.print("  Alt(m):");
+  Serial.print(Altitude / 100);
+  Serial.print("  Pressure(mm Hg):");
+  Serial.print(Pressure / 133.3);
+  Serial.print("  Temp:");
+  Serial.println(Temperature * 0.1);
   //с датчика dht22
-  Serial.print("Humidity: "); 
+  Serial.print("Humidity: ");
   Serial.print(h);
   Serial.print(" %\t");
-  Serial.print("Temperature: "); 
+  Serial.print("Temperature: ");
   Serial.print(t);
   Serial.print(" *C ");
   Serial.print(f);
   Serial.print(" *F\t");
   Serial.print("Heat index: ");
-  Serial.print(hi);
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
   Serial.println(" *F");
-  delay(2000); 
+  delay(2000);
 }
